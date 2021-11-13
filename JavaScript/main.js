ffd4a9914ebb39a079aee536f0ede4fa8b4e5797
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-11-13 14:04:06 -0300

    create menu burguer

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,3 +10,14 @@ function catador() {
 function biogas() {
   window.location.href = "https://www.gefbiogas.org.br/sobreoprojeto.html"
 }
+
+
+function navbar (){
+  const MenuBurguer = document.getElementById('navbar')
+      if(MenuBurguer.style.display === 'block'){
+        MenuBurguer.style.display = "none"
+      } else{
+        MenuBurguer.style.display = "block"
+      }
+
+}
